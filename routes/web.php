7bcb96e4018cc88c4086fc512b18367bc58e66be
Chwen <zzero.i.n.g@gmail.com>
2024-03-24 18:58:12 +0630

    breeze install

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\Article\ArticleController;
-use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,11 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::post('/products/{id}/update', [ProductController::class, 'update'])->name('products.update');
-Route::post('/products/{id}/delete', [ProductController::class, 'delete'])->name('products.delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('articles', ArticleController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
